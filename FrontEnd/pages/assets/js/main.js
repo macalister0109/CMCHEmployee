acd1f931584bb3f9596acd46d9f5f05be4b9ee5f
Gabriel Pedreros <165945370+Gabr1elpedreros@users.noreply.github.com>
2025-10-06 12:20:45 -0300

    d

--- FrontEnd/pages/assets/js/main.js
+++ FrontEnd/pages/assets/js/main.js
@@ -8,3 +8,11 @@
     console.log("Idioma seleccionado:", lang);
     //agregar lógica para cambiar el idioma
   }
+
+    function cambiarIdioma(idioma) {
+    if (idioma === 'es') {
+      window.location.href = 'main.html';
+    } else if (idioma === 'en') {
+      window.location.href = 'main_en.html';
+    }
+  }
