4c4f92d60ec4c0bb514c5d1d1a84c45388083943
Panconhu3vo <panconhuevo729@gmail.com>
2025-10-10 11:49:41 -0300

    feat: Agrega formularios de registro, y pantalla de inicio

---
+++ App/CMCHEmployee/src/screens/ProfileScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import Header from "../../components/Header";
+import { styles } from "./styles";
+
+export default function ProfileScreen() {
+    return (
+        <View style={styles.screen}>
+            <Header />
+        </View>
+    );
+}
