954df11dfe398c6e23d0cb1837026476a8f2550a
Panconhu3vo <panconhuevo729@gmail.com>
2025-11-05 22:34:01 -0300

    feat: agrega formulario login

---
+++ App/CMCH-Bag/src/components/Label/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import useStyles from "./styles";
+import useAppTheme from "../../context/ThemeContext";
+
+interface Props {}
+
+export default function Label() {
+    return <View></View>;
+}
