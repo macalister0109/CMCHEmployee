6a1ff0bd0d2a54dc44e52ad094f3fe8f1601e20e
Panconhu3vo <panconhuevo729@gmail.com>
2025-11-07 10:38:19 -0300

    delete: elimina version 1 de aplicacion

--- App/CMCH-Bag/src/screens/TabNavigationCompany/ProfileScreen/index.tsx
+++ App/CMCH-Bag/src/screens/TabNavigationCompany/ProfileScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Header from "../../../components/Header";
 import useStyles from "./styles";
@@ -11,6 +11,14 @@ export default function ProfileScreen() {
     const styles = useStyles();
     const copiar = () => {
         Clipboard.setStringAsync(profileCompany.email);
+    };
+    const abrirSitio = () => {
+        let url = profileCompany.website;
+        // Si el sitio no tiene "http", lo añadimos para evitar errores
+        if (!url.startsWith("http")) {
+            url = "https://" + url;
+        }
+        Linking.openURL(url);
     };
     return (
         <View style={styles.screen}>
@@ -45,9 +53,15 @@ export default function ProfileScreen() {
                     <Text style={styles.location}>
                         {profileCompany.location}
                     </Text>
-                    <Text style={styles.location}>
-                        {profileCompany.website}
-                    </Text>
+                    <TouchableOpacity onPress={abrirSitio}>
+                        <Text
+                            style={[
+                                styles.location,
+                                { textDecorationLine: "underline" },
+                            ]}>
+                            {profileCompany.website}
+                        </Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.descriptionContainer}>
                     <Text style={styles.title}>Descripción</Text>
